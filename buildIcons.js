191f71d1ac05f887a931c358a0cbb1614b72c66d
J.T. Sage <jtsage+datebox@gmail.com>
2024-01-18 14:43:48 -0500

    PNG and SVG bits are done

--- buildIcons.js
+++ buildIcons.js
@@ -11,20 +11,43 @@
 
 const path        = require('node:path')
 const buildTools  = require('./lib/buildTools.js')
+const log         = buildTools.cLog
 const { program } = require('commander')
 
 program
 	.name('buildIcons')
 	.description('Build Icon Files from SVG originals')
 	.option('-f, --folder <location>', 'Location of input SVG files')
+	.option('--no-svg', 'Do not write optimized SVG files')
+	.option('--no-atlas', 'Do not write PNG atlas files')
+	.option('--no-png', 'Do not write PNG files (required for atlas)')
+	.option('--no-js', 'Do not write javaScript files')
 
 program.parse()
 
 const options = program.opts()
 
 const thisSVGPath = options.folder ?? path.join(__dirname, 'svg_original')
+const fileList    = buildTools.getSVGInput(thisSVGPath)
 
+if ( options.svg )                    { log(buildTools.prepDist('SVG')) }
+if ( options.png || options.atlas )   { log(buildTools.prepDist('PNG')) }
+if ( options.atlas )                  { log(buildTools.prepDist('ATLAS')) }
+if ( options.js )                     { log(buildTools.prepDist('JS')) }
 
-console.log(buildTools.getSVGInput(thisSVGPath))
+for ( const thisFile of fileList.list.slice(0, 2) ) {
+	const thisSVGLoader = buildTools.writeSVG(thisFile, options.svg, fileList.len)
+	const thisSVG       = thisSVGLoader.data
+
+	log(thisSVGLoader.log)
+
+	if ( thisSVG === null ) { continue }
+
+	if ( options.png || options.atlas ) {
+		log(buildTools.writePNG(thisFile, thisSVG, fileList.len))
+	}
+}
+
+// console.log(options)
 
 // buildTools.prepDist('PNG')
